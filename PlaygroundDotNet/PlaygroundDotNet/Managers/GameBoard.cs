f8d7971bd2ecf636e0b5faf7c8b3fea9ed1d250f
Joshua Chen <jaysealearn@hotmail.com>
2019-12-05 18:50:15 +1300

    feat: Gameboard manages the game on a high level

---
+++ PlaygroundDotNet/PlaygroundDotNet/Managers/GameBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet.Managers
+{
+    public class GameBoard
+    {
+        private PrimaryPlayer _Player;
+
+        public GameBoard()
+        {
+            _Player = PrimaryPlayer.Instance;
+        }
+
+    }
+}
