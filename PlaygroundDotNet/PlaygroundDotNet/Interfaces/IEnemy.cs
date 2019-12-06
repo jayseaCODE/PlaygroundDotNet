b5fe01c4744e4e57d1de6ecf2ccf1d4d2bf2e91a
Joshua Chen <jaysealearn@hotmail.com>
2019-12-06 17:09:57 +1300

    feat: interface to describe an Enemy and its functionality

---
+++ PlaygroundDotNet/PlaygroundDotNet/Interfaces/IEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public interface IEnemy
+    {
+        int Health { get; set; }
+        int Level { get; }
+        int OvertimeDamage { get; set; }
+        int Armour { get; set; }
+        bool Paralyzed { get; set; }
+        int ParalyzedRoundsDuration { get; set; }
+        void Attack(PrimaryPlayer primaryPlayer);
+        void Defend(PrimaryPlayer primaryPlayer);
+    }
+}
