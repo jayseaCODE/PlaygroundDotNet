3d2e5b4da9c54d237a15ec6e617543544b70ac4c
Joshua Chen <jaysealearn@hotmail.com>
2019-12-06 17:11:18 +1300

    feat: Gameboard utilising EnemyFactory

--- PlaygroundDotNet/PlaygroundDotNet/Managers/GameBoard.cs
+++ PlaygroundDotNet/PlaygroundDotNet/Managers/GameBoard.cs
@@ -7,11 +7,18 @@ namespace PlaygroundDotNet.Managers
     public class GameBoard
     {
         private PrimaryPlayer _Player;
+        private EnemyFactory _EnemyFactory;
 
         public GameBoard()
         {
             _Player = PrimaryPlayer.Instance;
         }
 
+        public void PlayArea(int level)
+        {
+            List<IEnemy> enemies = new List<IEnemy>();
+            enemies.Add(_EnemyFactory.SpawnZombie(level));
+            enemies.Add(_EnemyFactory.SpawnWerewolf(level));
+        }
     }
 }
