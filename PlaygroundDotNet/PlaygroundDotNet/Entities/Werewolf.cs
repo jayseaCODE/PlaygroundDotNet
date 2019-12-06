509226b5c69d74d12b208ef1f7899f75f0800f1f
Joshua Chen <jaysealearn@hotmail.com>
2019-12-06 17:10:16 +1300

    feat: types of enemies that are IEnemy

---
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/Werewolf.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    /// <summary>
+    /// Werewolf can implement more interfaces for werewolf-specific functionalities that other Enemies do not have, in the future
+    /// </summary>
+    public class Werewolf : IEnemy
+    {
+        // Backing fields
+        private int _health;
+        private readonly int _level;
+        // Properties
+        public int Health { get => _health; set => _health = value; }
+        public int Level => _level;
+
+        public int OvertimeDamage { get; set; }
+        public int Armour { get; set; }
+        public bool Paralyzed { get; set; }
+        public int ParalyzedRoundsDuration { get; set; }
+        public Werewolf(int health, int level)
+        {
+            _health = health;
+            _level = level;
+        }
+        public void Attack(PrimaryPlayer primaryPlayer)
+        {
+            Console.WriteLine("Werewolf attacks " + primaryPlayer.Name);
+        }
+
+        public void Defend(PrimaryPlayer primaryPlayer)
+        {
+            Console.WriteLine("Werewolf defends from " + primaryPlayer.Name);
+        }
+    }
+}
---
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/Zombie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    /// <summary>
+    /// Zombie can implement more interfaces for zombie-specific functionalities that other Enemies do not have, in the future
+    /// </summary>
+    public class Zombie : IEnemy
+    {
+        // Backing fields
+        private int _health;
+        private readonly int _level;
+        // Properties
+        public int Health { get => _health; set => _health = value; }
+        public int Level => _level;
+
+        public int OvertimeDamage { get; set; }
+        public int Armour { get; set; }
+        public bool Paralyzed { get; set; }
+        public int ParalyzedRoundsDuration { get; set; }
+
+        public Zombie(int health, int level)
+        {
+            _health = health;
+            _level = level;
+        }
+        public void Attack(PrimaryPlayer primaryPlayer)
+        {
+            Console.WriteLine("Zombie attacks " + primaryPlayer.Name);
+        }
+
+        public void Defend(PrimaryPlayer primaryPlayer)
+        {
+            Console.WriteLine("Zombie defends from " + primaryPlayer.Name);
+        }
+    }
+}
