9069e052a834ddede4c2329d08dfaec5540c7d0f
Joshua Chen <jaysealearn@hotmail.com>
2019-12-06 17:11:02 +1300

    feat: Factory who single responsibility is to produce Enemies

---
+++ PlaygroundDotNet/PlaygroundDotNet/EnemyFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public class EnemyFactory
+    {
+        public Werewolf SpawnWerewolf(int areaLevel)
+        {
+            if (areaLevel < 5)
+            {
+                return new Werewolf(100, 10);
+            }
+            else
+            {
+                return new Werewolf(100, 20);
+            }
+        }
+
+        public Zombie SpawnZombie(int areaLevel)
+        {
+            if (areaLevel < 2)
+            {
+                return new Zombie(100, 2);
+            }
+            else
+            {
+                return new Zombie(100, 10);
+            }
+        }
+    }
+}
