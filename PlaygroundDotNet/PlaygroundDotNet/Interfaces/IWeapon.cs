bac686d20d486e3f9df58d83c815de86aa401bd7
Joshua Chen <jaysealearn@hotmail.com>
2019-12-09 15:50:26 +1300

    create a Weapon Interface for Player to equip

---
+++ PlaygroundDotNet/PlaygroundDotNet/Interfaces/IWeapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public interface IWeapon
+    {
+        int Damage { get; }
+        void Attack(IEnemy enemy);
+    }
+}
