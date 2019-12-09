01735c61110991a4b6f7b6967652d8afda0ba470
Joshua Chen <jaysealearn@hotmail.com>
2019-12-09 15:50:53 +1300

    feat: Sword that functions as a weapon

---
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/Sword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public class Sword : IWeapon
+    {
+        private int _damage;
+        public int Damage => _damage;
+        private int _armourDamage;
+        public int ArmourDamage => _armourDamage;
+
+        public Sword(int damage, int armourDamage)
+        {
+            _damage = damage;
+            _armourDamage = armourDamage;
+        }
+
+        public void Attack(IEnemy enemy)
+        {
+            enemy.TakeDamage(_damage, _armourDamage);
+        }
+    }
+}
