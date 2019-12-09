315d83d9004bbbcecf1e4e142c0340d3ddeed3b6
Joshua Chen <jaysealearn@hotmail.com>
2019-12-09 15:51:56 +1300

    feat: Fire and Ice Staffs that functions as magic staff weapons

---
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/FireStaff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public class FireStaff : IStaff
+    {
+        private int _damage;
+        public int Damage => _damage;
+        private int _damageOutputOvertime;
+        public int DamageOutputOvertime => _damageOutputOvertime;
+
+        public FireStaff(int damage, int fireDamage)
+        {
+            _damage = damage;
+            _damageOutputOvertime = fireDamage;
+        }
+
+        public void Attack(IEnemy enemy)
+        {
+            if (enemy.Armour > 0)
+            {
+                var leftoverArmour = enemy.Armour - _damage;
+                if (leftoverArmour < 0)
+                {
+                    enemy.Armour = 0;
+                }
+                else
+                {
+                    enemy.Armour = leftoverArmour;
+                }
+            }
+            else
+            {
+                enemy.Health -= _damage;
+            }
+            enemy.OvertimeDamageTaken = _damageOutputOvertime;
+        }
+    }
+}
---
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/IceStaff.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public class IceStaff : IStaff
+    {
+        private int _paralyzedRoundDuration;
+        private int _damageOutputOverTime;
+        public int DamageOutputOvertime => _damageOutputOverTime;
+        private int _damage;
+        public int Damage => _damage;
+
+        public IceStaff(int damage, int iceDamage, int paralyzedRoundDuration)
+        {
+            _damage = damage;
+            _damageOutputOverTime = iceDamage;
+            _paralyzedRoundDuration = paralyzedRoundDuration;
+        }
+
+        public void Attack(IEnemy enemy)
+        {
+            if (enemy.Armour > 0)
+            {
+                var leftoverArmour = enemy.Armour - _damage;
+                if (leftoverArmour < 0)
+                {
+                    enemy.Armour = 0;
+                }
+                else
+                {
+                    enemy.Armour = leftoverArmour;
+                }
+            }
+            else
+            {
+                enemy.Health -= _damage;
+            }
+            enemy.OvertimeDamageTaken = -_damageOutputOverTime;
+            enemy.Paralyzed = true;
+            enemy.ParalyzedRoundsDuration = _paralyzedRoundDuration;
+        }
+    }
+}
