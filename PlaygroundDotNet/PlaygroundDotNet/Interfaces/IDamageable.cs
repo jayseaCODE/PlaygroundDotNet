0d1454b969b14a50b7cd0a48c3029d61db57250c
Joshua Chen <jaysealearn@hotmail.com>
2019-12-09 19:07:04 +1300

    Damageable interface for objects that can receive damage

---
+++ PlaygroundDotNet/PlaygroundDotNet/Interfaces/IDamageable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public interface IDamageable
+    {
+        void TakeDamage(int dealtDamage, int dealtArmourDamage);
+    }
+}
