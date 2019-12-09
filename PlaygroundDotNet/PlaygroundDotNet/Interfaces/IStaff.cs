bebf31e16c8c34bb47531ef656d0d98452c76d3a
Joshua Chen <jaysealearn@hotmail.com>
2019-12-09 15:51:24 +1300

    create a Staff interface for Magical Weapons, extends Weapon interface

---
+++ PlaygroundDotNet/PlaygroundDotNet/Interfaces/IStaff.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet
+{
+    public interface IStaff : IWeapon
+    {
+        int DamageOutputOvertime { get; }
+    }
+}
