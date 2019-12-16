761150b7a11cb5068cbef4e43279513db3b2a752
Joshua Chen <jaysealearn@hotmail.com>
2019-12-17 12:42:12 +1300

    create an interface and implementation of this ICardsService

---
+++ PlaygroundDotNet/WebApi/Services/CardsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+
+namespace WebApi.Services
+{
+    public class CardsService : ICardsService
+    {
+        public IEnumerable<Card> GetCards()
+        {
+            return new List<Card>()
+            {
+                new Card { Name="Shadow Wraith", Attack = 90, Defense = 50},
+            };
+        }
+    }
+}
