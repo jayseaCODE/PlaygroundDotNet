463678463efcba83f9fa23f264ebdd7ffba33b39
Joshua Chen <jaysealearn@hotmail.com>
2019-12-17 12:43:08 +1300

    feat: GetAll cards by hitting api/Cards default route

---
+++ PlaygroundDotNet/WebApi/Controllers/CardsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cards")]
+    public class CardsController : Controller
+    {
+        private ICardsService _cardsService;
+
+        public CardsController(ICardsService cardsService)
+        {
+            _cardsService = cardsService;
+        }
+
+        // GET: api/Cards
+        [HttpGet("")]
+        public IEnumerable<Card> GetAll()
+        {
+            return _cardsService.GetCards();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
