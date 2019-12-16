5470607aea758086d5fe019be06fb3b5eed19168
Joshua Chen <jaysealearn@hotmail.com>
2019-12-17 12:50:31 +1300

    registers the ICardsService as a Singleton in web api container

--- PlaygroundDotNet/WebApi/Startup.cs
+++ PlaygroundDotNet/WebApi/Startup.cs
@@ -27,6 +27,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<ICardsService, CardsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
