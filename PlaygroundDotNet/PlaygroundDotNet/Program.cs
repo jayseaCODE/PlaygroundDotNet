ae95154747e5966532bbe3a76e060de00bd178eb
Joshua Chen <jaysealearn@hotmail.com>
2019-12-18 16:23:55 +1300

    method to test connection to server through web api

--- PlaygroundDotNet/PlaygroundDotNet/Program.cs
+++ PlaygroundDotNet/PlaygroundDotNet/Program.cs
@@ -10,6 +10,7 @@ namespace PlaygroundDotNet
             //SingleResponsibilityExample.Run(args);
             try
             {
+                TestWebApiConnection().Wait();
                 GameBoard gameBoard = new GameBoard();
                 gameBoard.PlayArea(1);
                 Console.ReadKey();
@@ -18,6 +19,31 @@ namespace PlaygroundDotNet
             {
                 Console.WriteLine("Failed to initialize game.");
             }
+        static async Task TestWebApiConnection()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            int retryCurrentDelay = 100; // 100 milliseconds
+            int retryMaxDelay = 1000; // 1 seconds
+            bool retry = false, tryGetResponse = true;
+
+            while (tryGetResponse)
+            {
+                try
+                {
+                    response = await httpClient.GetAsync("https://localhost:44365/api/cards");
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK) return;
+                    else
+                    {
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to connect to server.");
+                }
+            }
+        }
 
             GameBoard gameBoard = new GameBoard();
             gameBoard.PlayArea(1);
