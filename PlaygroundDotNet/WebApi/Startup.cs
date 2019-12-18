89dfb80d7b315f08853beef9ce7f3f851182474d
Joshua Chen <jaysealearn@hotmail.com>
2019-12-18 16:30:24 +1300

    change the CardsService to have a transient service lifetime

--- PlaygroundDotNet/WebApi/Startup.cs
+++ PlaygroundDotNet/WebApi/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICardsService, CardsService>();
+            services.AddTransient<ICardsService, CardsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
