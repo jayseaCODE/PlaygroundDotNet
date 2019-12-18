93141f7457b0fa0b88ccbd0aec00069fc3345779
Joshua Chen <jaysealearn@hotmail.com>
2019-12-18 16:23:18 +1300

    add a try and catch block surrounding game init

--- PlaygroundDotNet/PlaygroundDotNet/Program.cs
+++ PlaygroundDotNet/PlaygroundDotNet/Program.cs
@@ -8,6 +8,16 @@ namespace PlaygroundDotNet
         static void Main(string[] args)
         {
             //SingleResponsibilityExample.Run(args);
+            try
+            {
+                GameBoard gameBoard = new GameBoard();
+                gameBoard.PlayArea(1);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to initialize game.");
+            }
 
             GameBoard gameBoard = new GameBoard();
             gameBoard.PlayArea(1);
