c1cf1c138e53f5c71e26dc90da453cd09ede2fe4
Joshua Chen <jaysealearn@hotmail.com>
2019-12-19 13:39:57 +1300

    allow PrimaryPlayer to hold a List of Card/s

--- PlaygroundDotNet/PlaygroundDotNet/Entities/PrimaryPlayer.cs
+++ PlaygroundDotNet/PlaygroundDotNet/Entities/PrimaryPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -20,6 +21,8 @@ namespace PlaygroundDotNet
         public int Armour { get; set; }
         public int Health { get; set; }
         public IWeapon Weapon { get; set; }
+        public List<Card> Cards { get; set; }
+
         /// <summary>
         /// Default Constructor called in Static Constructor
         /// </summary>
