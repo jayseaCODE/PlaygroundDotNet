f6baa11ef84f105844e873b594c169d928585cde
Joshua Chen <jaysealearn@hotmail.com>
2019-12-27 14:24:25 +1300

    feat: Decorator base class that inherits from Card

---
+++ PlaygroundDotNet/Common/CardDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class CardDecorator : Card
+    {
+        protected Card _card;
+        public CardDecorator(Card card, string name, int attack, int defense) : base(name, attack, defense)
+        {
+            _card = card;
+        }
+
+        public override string Name
+        {
+            get => $"{_card.Name}, {_name}";
+        }
+
+        public override int Attack => _card.Attack + _attack;
+
+        public override int Defense => _card.Defense + _defense;
+    }
+}
