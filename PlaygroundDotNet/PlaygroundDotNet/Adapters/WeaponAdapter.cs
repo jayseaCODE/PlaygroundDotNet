33842c75513ce4bd9d297643dfc91eec75ee93bc
Joshua Chen <jaysealearn@hotmail.com>
2019-12-30 15:16:59 +1300

    class that adapts any interface to an IWeapon to use

---
+++ PlaygroundDotNet/PlaygroundDotNet/Adapters/WeaponAdapter.cs
@@ -0,0 +1,23 @@
+﻿using PlaygroundDotNet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet.Adapters
+{
+    public class WeaponAdapter : IWeapon
+    {
+        private ISpaceWeapon _spaceWeapon;
+        public WeaponAdapter(ISpaceWeapon spaceWeapon)
+        {
+            _spaceWeapon = spaceWeapon;
+        }
+
+        public int Damage => _spaceWeapon.ImpactDamage + _spaceWeapon.LaserDamage;
+
+        public void Attack(IEnemy enemy)
+        {
+            enemy.Health -= _spaceWeapon.Shoot();
+        }
+    }
+}
