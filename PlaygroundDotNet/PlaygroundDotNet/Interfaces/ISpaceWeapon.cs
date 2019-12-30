13156d6b5a98d34b89ab11b769ec47a07628e93e
Joshua Chen <jaysealearn@hotmail.com>
2019-12-30 15:16:25 +1300

    feat: an interface that is different to IWeapon but similar in concept

---
+++ PlaygroundDotNet/PlaygroundDotNet/Interfaces/ISpaceWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundDotNet.Interfaces
+{
+    /// <summary>
+    /// Used for the purpose of converting an <see cref="IWeapon"/> to this <see cref="ISpaceWeapon"/> by using a <see cref="Adapters.WeaponAdapter"/>
+    /// </summary>
+    public interface ISpaceWeapon
+    {
+        int ImpactDamage { get; }
+        int LaserDamage { get; }
+        int MissChance { get; }
+        int Shoot();
+    }
+}
